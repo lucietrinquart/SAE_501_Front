52139728f703461cdd135aa7851b3cf59e05f299
lucietrinquart <118741664+lucietrinquart@users.noreply.github.com>
2024-10-09 18:47:43 +0200

    Update user_list ajout semester onglet

--- src/app/pages/user-list/user-list.component.ts
+++ src/app/pages/user-list/user-list.component.ts
@@ -3,34 +3,50 @@ import { ApiService } from "../../shared/services/api.service";
 import { User } from "../../shared/interfaces/user";
 import { UserWorkload } from '../../shared/interfaces/user-workload';
 import { ResourceList } from '../../shared/interfaces/resources';
+import { Semester } from '../../shared/interfaces/semester';
 
 @Component({
   selector: 'app-user-list',
   templateUrl: './user-list.component.html',
-  styleUrls: ['./user-list.component.scss'] 
+  styleUrls: ['./user-list.component.scss']
 })
 export class UserListComponent {
-  users: User[] = [];  // Modifié en tableau de User
+  users: User[] = [];
   userworkload: UserWorkload[] = [];
   resource: ResourceList[] = [];
+  semestre: Semester[] = [];
+
+  // Variable pour stocker le semestre sélectionné
+  selectedSemester: number = 1;  // Sélectionne le semestre 1 par défaut
 
   constructor(private apiService: ApiService) {
     // Appel API pour récupérer les utilisateurs
     this.apiService.requestApi(`/user`)
-      .then((response: User[]) => {  // Supposons que l'API retourne un tableau d'utilisateurs
+      .then((response: User[]) => {
         this.users = response;
       });
 
-    // Appel API pour récupérer la charge de travail de l'utilisateur
-    this.apiService.requestApi(`/resource-workload`)
+    // Appel API pour récupérer la charge de travail
+    this.apiService.requestApi(`/resource/workload`)
       .then((response: UserWorkload[]) => {
         this.userworkload = response;
       });
 
-        // Appel API pour récupérer la charge de travail de l'utilisateur
-        this.apiService.requestApi(`/resource`)
-        .then((response: ResourceList[]) => {
-          this.resource = response;
-        });
+    // Appel API pour récupérer les ressources
+    this.apiService.requestApi(`/resource`)
+      .then((response: ResourceList[]) => {
+        this.resource = response;
+      });
+
+    // Appel API pour récupérer les semestres
+    this.apiService.requestApi(`/semesters`)
+      .then((response: Semester[]) => {
+        this.semestre = response;
+      });
+  }
+
+  // Méthode pour changer le semestre sélectionné
+  selectSemester(semestreId: number) {
+    this.selectedSemester = semestreId;
   }
 }
