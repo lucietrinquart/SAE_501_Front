cee7e4a3e08005fd3302e600220866101677ec34
lucietrinquart <118741664+lucietrinquart@users.noreply.github.com>
2024-10-09 16:00:16 +0200

    Changement style user-list

--- src/app/pages/user-list/user-list.component.ts
+++ src/app/pages/user-list/user-list.component.ts
@@ -2,27 +2,35 @@ import { Component } from '@angular/core';
 import { ApiService } from "../../shared/services/api.service";
 import { User } from "../../shared/interfaces/user";
 import { UserWorkload } from '../../shared/interfaces/user-workload';
+import { ResourceList } from '../../shared/interfaces/resources';
 
 @Component({
   selector: 'app-user-list',
   templateUrl: './user-list.component.html',
-  styleUrls: ['./user-list.component.scss']  // Correction de "styleUrl" en "styleUrls"
+  styleUrls: ['./user-list.component.scss'] 
 })
 export class UserListComponent {
   users: User[] = [];  // Modifié en tableau de User
   userworkload: UserWorkload[] = [];
+  resource: ResourceList[] = [];
 
   constructor(private apiService: ApiService) {
     // Appel API pour récupérer les utilisateurs
-    this.apiService.requestApi(`/prof`)
+    this.apiService.requestApi(`/user`)
       .then((response: User[]) => {  // Supposons que l'API retourne un tableau d'utilisateurs
         this.users = response;
       });
 
     // Appel API pour récupérer la charge de travail de l'utilisateur
-    this.apiService.requestApi(`/user_workload`)
+    this.apiService.requestApi(`/resource-workload`)
       .then((response: UserWorkload[]) => {
         this.userworkload = response;
       });
+
+        // Appel API pour récupérer la charge de travail de l'utilisateur
+        this.apiService.requestApi(`/resource`)
+        .then((response: ResourceList[]) => {
+          this.resource = response;
+        });
   }
 }
