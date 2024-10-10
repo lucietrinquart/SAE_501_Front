b5425b30aeb247cd8cdad3654ef847bcc92aaa07
lucietrinquart <118741664+lucietrinquart@users.noreply.github.com>
2024-10-10 09:44:53 +0200

    Update affichage onglet semester

--- src/app/pages/user-list/user-list.component.ts
+++ src/app/pages/user-list/user-list.component.ts
@@ -16,8 +16,8 @@ export class UserListComponent {
   resource: ResourceList[] = [];
   semestre: Semester[] = [];
 
-  // Variable pour stocker le semestre sélectionné
-  selectedSemester: number = 1;  // Sélectionne le semestre 1 par défaut
+  selectedSemester: number = 1;
+  selectedProfessor: number | null = null; // C'est bien un nombre ou null
 
   constructor(private apiService: ApiService) {
     // Appel API pour récupérer les utilisateurs
@@ -45,8 +45,35 @@ export class UserListComponent {
       });
   }
 
-  // Méthode pour changer le semestre sélectionné
+  // Sélection du semestre
   selectSemester(semestreId: number) {
     this.selectedSemester = semestreId;
   }
+
+  // Sélection ou désélection du professeur
+  // selectProfessor(userId: number) {
+  //   // Vérifie si le selectedProfessor est bien un nombre avant de faire la comparaison
+  //   if (this.selectedProfessor === userId) {
+  //     this.selectedProfessor = null; // Si on clique sur le même professeur, on désélectionne
+  //   } else {
+  //     this.selectedProfessor = userId; // Sélectionne le professeur
+  //   }
+  // }
+
+  // // Méthode pour filtrer les ressources
+  // filterResources(resource: ResourceList): boolean {
+  //   const semesterMatch = +resource.semester_id === this.selectedSemester;
+
+  //   // Si aucun professeur n'est sélectionné, on retourne juste les ressources du semestre
+  //   if (this.selectedProfessor === null) {
+  //     return semesterMatch;
+  //   }
+
+  //   // Vérifie si le professeur sélectionné a une ressource correspondante
+  //   const professorMatch = this.userworkload.some(uw => uw.id_user === this.selectedProfessor && uw.id_resource === resource.id
+  //   );
+    
+  //   // Retourne true seulement si les deux conditions sont vérifiées
+  //   return semesterMatch && professorMatch;
+  // }
 }
