08db28d03def423e513d374a1d7b5ae7b2023fb3
Nathan34725 <nathanamat34@gmail.com>
2024-10-10 11:35:35 +0200

    Feat:Resource_by_Semester

--- src/app/pages/resource-list/resource-list.component.ts
+++ src/app/pages/resource-list/resource-list.component.ts
@@ -7,16 +7,39 @@ import { ResourceList } from "../../shared/interfaces/resources";
   templateUrl: './resource-list.component.html',
   styleUrl: './resource-list.component.scss'
 })
-
 export class ResourceListComponent {
-  resource: ResourceList[] = [];
+  public resources: ResourceList[] = [];
+  public filteredResources: ResourceList[] = [];
+  public selectedSemester: number | null = null;
+  public semesters: number[] = [1, 2, 3, 4, 5, 6];
 
   constructor(private apiService: ApiService) {
-    // Appel API pour récupérer les utilisateurs
-    this.apiService.requestApi(`/resource`)
-      .then((response: ResourceList[]) => {  // Supposons que l'API retourne un tableau d'utilisateurs
-        this.resource = response;
-      });
+    this.loadResources();
+  }
 
+  private loadResources(): void {
+    this.apiService.requestApi(`/resource`)
+      .then((response: ResourceList[]) => {
+        console.log('API Response:', response);
+        this.resources = response;
+        this.filteredResources = response;
+      })
+      .catch(error => {
+        console.error('Error loading resources:', error);
+      });
+  }
+
+  public filterBySemester(semesterId: number | null): void {
+    console.log('Filtering by semester:', semesterId);
+    this.selectedSemester = semesterId;
+    
+    if (semesterId === null) {
+      this.filteredResources = this.resources;
+    } else {
+      this.filteredResources = this.resources.filter(resource => 
+        resource.semester_id === semesterId
+      );
+    }
+    console.log('Filtered resources:', this.filteredResources);
   }
 }
