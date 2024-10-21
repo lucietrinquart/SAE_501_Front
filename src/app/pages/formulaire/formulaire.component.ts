9051b115d68139cfff094d2cbfefb4c3be8e01e4
lucietrinquart <118741664+lucietrinquart@users.noreply.github.com>
2024-10-21 13:51:52 +0200

    create formulaire component

--- src/app/pages/formulaire/formulaire.component.ts
+++ src/app/pages/formulaire/formulaire.component.ts
@@ -1,9 +1,8 @@
 import { Component } from '@angular/core';
 import { User } from "../../shared/interfaces/user";
 import { ApiService } from "../../shared/services/api.service";
-import { ActivatedRoute } from "@angular/router";
+import { Router } from "@angular/router";
 
-// Définissons un type pour un nouvel utilisateur
 type NewUser = Omit<User, 'id'> & { id?: number };
 
 @Component({
@@ -19,13 +18,32 @@ export class FormulaireComponent {
     email: '',
     password: ''
   };
+  loginUser = {
+    username: '',
+    role: '',
+    email: '',
+    password: ''
+  };
+  isLoginMode = false;
 
   constructor(
     private apiService: ApiService,
-    private route: ActivatedRoute
+    private router: Router,
   ) {}
 
+  toggleMode() {
+    this.isLoginMode = !this.isLoginMode;
+  }
+
   onSubmit() {
+    if (this.isLoginMode) {
+      this.login();
+    } else {
+      this.register();
+    }
+  }
+
+  register() {
     this.apiService.postApi('/user/create', this.newUser).then((response: User) => {
       console.log('Nouvel utilisateur créé:', response);
       this.users.push(response);
@@ -34,9 +52,22 @@ export class FormulaireComponent {
         role: '',
         email: '',
         password: ''
-      }; 
+      };
     }).catch(error => {
-      console.error('Erreur lors de la création de l utilisateur:', error);
+      console.error('Erreur lors de la création de l\'utilisateur:', error);
     });
   }
-}
+
+  login() {
+    this.apiService.postApi('/user/login', this.loginUser).then((response: any) => {
+      if (response && response.user) {
+        console.log('Connexion réussie:', response.user);
+        this.router.navigate(['/user']);  // Redirige vers la page utilisateur
+      } else {
+        console.error('Erreur de connexion:', response.message);
+      }
+    }).catch(error => {
+      console.error('Erreur de connexion:', error);
+    });
+  }
+  }
