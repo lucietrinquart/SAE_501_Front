b98fbe260cb089e9f2811d656c01f6eedecf0785
Nathan34725 <nathanamat34@gmail.com>
2024-10-22 17:13:24 +0200

    feat:New_UI

--- tailwind.config.js
+++ tailwind.config.js
@@ -10,7 +10,8 @@ module.exports = {
         'fond_page': '#1E1E1E',
         'fond_section1': '#303139',
         'police': '#000000',
-        'fond_section2': '#4D4F59'
+        'fond_section2': '#4D4F59',
+
       }
     },
   },
