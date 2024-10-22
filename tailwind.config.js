0a57b31cf114d612fccf27d0ca5e9ed55a21b70f
Nathan34725 <nathanamat34@gmail.com>
2024-10-22 11:05:21 +0200

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,7 +4,15 @@ module.exports = {
     "./src/**/*.{html,ts}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        'blanc': '#FFFFFF',
+        'fond_page': '#1E1E1E',
+        'fond_section1': '#303139',
+        'police': '#000000',
+        'fond_section2': '#4D4F59'
+      }
+    },
   },
   plugins: [],
 }
