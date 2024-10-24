874aa3be40c5ac48a759c950c81f18a07fb43ffe
Juuhey <jujupantel@gmail.com>
2024-10-24 10:06:46 +0200

    add. component for pdf gen

--- src/app/pages/pdf-form-component/pdf-form-component.component.ts
+++ src/app/pages/pdf-form-component/pdf-form-component.component.ts
@@ -14,14 +14,26 @@ export class PdfFormComponent {
 
   async onSubmit() {
     try {
-      const response = await this.apiService.requestApi('/generate-pdf', 'POST', { name: this.name, email: this.email }, undefined, {}, 'blob');
-      
-      // Supposons que la réponse est un Blob (PDF)
-      const file = new Blob([response], { type: 'application/pdf' });
-      const fileURL = URL.createObjectURL(file);
-      
-      // Ouvrir le fichier PDF dans une nouvelle fenêtre
-      window.open(fileURL);
+      const response = await this.apiService.requestApi(
+        '/pdf/generate',
+        'POST',
+        { name: this.name, email: this.email }
+      );
+     
+      if (response.file_id) {
+        // 
+        const pdfResponse = await this.apiService.requestApi(
+          `/pdf/${response.file_id}`, 
+          'GET'
+        );
+        
+        const blob = new Blob([pdfResponse], { type: 'application/pdf' });
+        const url = window.URL.createObjectURL(blob);
+        
+        window.open(url);
+
+        setTimeout(() => window.URL.revokeObjectURL(url), 100);
+      }
     } catch (error) {
       console.error('Erreur lors de la génération du PDF', error);
     }
