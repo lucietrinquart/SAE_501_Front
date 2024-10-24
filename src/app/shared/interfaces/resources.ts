6a3eb24c04f656b024e890356bf6d951fa05beed
ZyFou <zyfodexe@gmail.com>
2024-10-24 11:37:28 +0200

    Feat : Added ref teacher gestion

--- src/app/shared/interfaces/resources.ts
+++ src/app/shared/interfaces/resources.ts
@@ -4,6 +4,7 @@ export interface ResourceList {
     semester_id: number;  // Changé de Semester[] à number
     description?: string;
     resource_type_id: number;
+    ref_teacher_id?: number;
     course?: string;
     vol_nat: number;
     vol_nat_tp: number;
