30eae54a5408c76ff9e90b63027cd54039fbb360
ZyFou <zyfodexe@gmail.com>
2024-10-25 14:21:17 +0200

    Feat :  c'est réparé

--- src/app/shared/nav-buttons/nav-buttons.component.ts
+++ src/app/shared/nav-buttons/nav-buttons.component.ts
@@ -1,13 +1,18 @@
 import { Component } from '@angular/core';
 import { ApiService } from "../../shared/services/api.service";
-
+import { Router } from '@angular/router';  // Import Router for navigation
 
 @Component({
   selector: 'app-nav-buttons',
   templateUrl: './nav-buttons.component.html',
-  styleUrl: './nav-buttons.component.scss'
+  styleUrls: ['./nav-buttons.component.scss']  // Corrected styleUrls
 })
 export class NavButtonsComponent {
 
+  constructor(private apiService: ApiService, private router: Router) { }  // Inject ApiService and Router
 
+  logout() {
+    this.apiService.logout();  // Call the logout function from ApiService
+    this.router.navigate(['/login']); // Redirect to the login page
+  }
 }
