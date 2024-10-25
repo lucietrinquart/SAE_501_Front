a29ebf42447a64bcaf0bde9787e1216030781848
ZyFou <zyfodexe@gmail.com>
2024-10-25 14:17:54 +0200

    Feat : Please fait logout lucie

---
+++ src/app/shared/nav-buttons/nav-buttons.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+import { ApiService } from "../../shared/services/api.service";
+
+
+@Component({
+  selector: 'app-nav-buttons',
+  templateUrl: './nav-buttons.component.html',
+  styleUrl: './nav-buttons.component.scss'
+})
+export class NavButtonsComponent {
+
+
+}
